48050dd43cad36a5425f0d2c3a5fb53a69c20060
hemensol <hemensol14@yahoo.com>
2023-05-18 13:20:01 +0300

    employee training ransack fixed

--- app/models/employee_training.rb
+++ app/models/employee_training.rb
@@ -5,4 +5,9 @@ class EmployeeTraining < ApplicationRecord
   belongs_to :training
 
   validates :season, presence: true
+
+  def self.ransackable_attributes(auth_object = nil)
+    ['category_id', 'created_at', 'id', 'employee_id', 'training_id', 'season','updated_at']
+  end
+
 end
