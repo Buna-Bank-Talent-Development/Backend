b9493f2289887108a483dc267c49a44be0fc4a41
Eyosiyas Mekbib <eyosi9000@gmail.com>
2023-05-20 20:43:02 +0300

    added uniquess validation to email

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,8 +1,9 @@
 class Employee < ApplicationRecord
   has_one_time_password
+  rolify
 
   validates :full_name, presence: true
-  validates :email, presence: true
+  validates :email, presence: true , uniqueness: true
   validates :department, presence: true
   validates :location, presence: true
 end
